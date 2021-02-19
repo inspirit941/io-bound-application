ad8fe1bc96dbdcac91549fd715e230f2ac5f919d
inspirit941 <inspirit941@gmail.com>
2021-02-19 13:30:27 +0900

    update to elasticSearch

--- src/main/java/class101/foo/io/PostController.java
+++ src/main/java/class101/foo/io/PostController.java
@@ -3,9 +3,6 @@ package class101.foo.io;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Sort;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -22,8 +19,8 @@ public class PostController {
     Producer producer;
     @Autowired
     ObjectMapper objectMapper;
-    @Autowired
-    PostCacheService postCacheService;
+//    @Autowired
+//    PostCacheService postCacheService;
 
     // 1. 글을 작성한다.
     @PostMapping("/post")
@@ -32,29 +29,34 @@ public class PostController {
         producer.sendTo(jsonObject);
         return post;
     }
+    @GetMapping("/search")
+    public List<Post> findPostsByContent(@RequestParam String content) {
+        return postRepository.findByContent(content);
+    }
 
     // 2. 글 목록을 페이징하여 반환
-    @GetMapping("/posts")
-    public Page<Post> getPostList(@RequestParam(defaultValue = "1") Integer page) {
-        // 첫 페이지 요청은 캐시로 처리.
-        if (page.equals(1)) {
-            return postCacheService.getFirstPostPage();
-        } else {
-            return postRepository.findAll(
-                    PageRequest.of(page - 1, PAGE_SIZE, Sort.by("id").descending())
-            );
-        }
-
-    }
+//    @GetMapping("/posts")
+//    public Page<Post> getPostList(@RequestParam(defaultValue = "1") Integer page) {
+//        // 첫 페이지 요청은 캐시로 처리.
+//        if (page.equals(1)) {
+//            return postCacheService.getFirstPostPage();
+//        } else {
+//            return postRepository.findAll(
+//                    PageRequest.of(page - 1, PAGE_SIZE, Sort.by("id").descending())
+//            );
+//        }
+//
+//    }
     // 3. 글 번호로 조회
-    @GetMapping("/post/{id}")
-    public Post getPostById(@PathVariable("id") Long id) {
-        return postRepository.findById(id).get();
-    }
+//    @GetMapping("/post/{id}")
+//    public Post getPostById(@PathVariable("id") Long id) {
+//        return postRepository.findById(id).get();
+//    }
     // 4. 글 내용으로 검색 -> 해당 내용이 포함된 모든 글
-    @GetMapping("/search")
-    public List<Post> getPostByContent(@RequestParam String content) {
-        return postRepository.findByContentContains(content);
-    }
+//    @GetMapping("/search")
+//    public List<Post> getPostByContent(@RequestParam String content) {
+//        return postRepository.findByContentContains(content);
+//    }
+
 
 }
